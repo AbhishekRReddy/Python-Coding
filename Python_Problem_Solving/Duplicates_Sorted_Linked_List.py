ae5d4f7be3bf2610066db94a14016c97e179ed72
AbhishekRReddy <arreddy1997@gmail.com>
2023-03-10 22:30:43 +0530

    Implemented the duplicates in the sorted linked lists

---
+++ Python_Problem_Solving/Duplicates_Sorted_Linked_List.py
@@ -0,0 +1,18 @@
+'''
+https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head):
+        current_node = head
+        while current_node and current_node.next:
+            if current_node.val == current_node.next.val:
+                current_node.next = current_node.next.next
+                continue
+            current_node = current_node.next
+        return head
--- Python_Problem_Solving/Merge_Sorted_Arrays_LL.py
+++ Python_Problem_Solving/Merge_Sorted_Arrays_LL.py
@@ -1,12 +1,12 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    def mergeTwoLists(self, list1,list2):
         pointer = ListNode()
         result_linked_list_head = pointer
         current_node1 = list1
