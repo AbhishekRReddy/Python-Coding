b1efb593a5cfa9a3cce47e993559ca5b6cd65d28
AbhishekRReddy <arreddy1997@gmail.com>
2023-03-12 13:02:20 +0530

    Function return the index where we can Search_Insert_Position

---
+++ Python_Problem_Solving/Search_Insert_Position.py
@@ -0,0 +1,30 @@
+'''
+https://leetcode.com/problems/search-insert-position/description/
+'''
+
+
+class Solution:
+    def binary_search(self, nums,target, low, high):
+        if low >= high:
+            if target<nums[0]:
+                return 0
+            elif target > nums[len(nums)-1]:
+                return len(nums)
+        mid =(low+high)//2
+        if nums[mid] == target:
+            return mid
+        elif target < nums[mid] and target > nums[mid-1]:
+            return mid
+        elif target > nums[mid] and target < nums[mid+1]:
+            return mid+1
+        else:
+            if target < nums[mid]:
+                return self.binary_search(nums,target,low,mid-1)
+            else:
+                return self.binary_search(nums,target,mid+1,high)
+
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        high = len(nums)-1
+        return self.binary_search(nums,target,0,high)
+
+    
