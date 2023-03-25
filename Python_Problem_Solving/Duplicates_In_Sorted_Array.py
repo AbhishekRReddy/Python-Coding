406963c4ecadee6a890de58a97d54caa391a7e5f
AbhishekRReddy <arreddy1997@gmail.com>
2023-03-25 23:08:42 +0530

    Implemented the optimal solution

--- Python_Problem_Solving/Duplicates_In_Sorted_Array.py
+++ Python_Problem_Solving/Duplicates_In_Sorted_Array.py
@@ -16,6 +16,39 @@ class Solution:
         return insert_pos
 
 
+'''Below is the two pointer method
+1. position is pointing to location where the new element can be inserted. 
+2. i is running pointer, which keeps running through the list.
+3. If the current number and next number is dissimilar then we can send the next number to the
+    position pointer where it was pointing.
+'''
+
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> int:
+        position = 1
+        for i in range(len(nums)-1):
+            if nums[i] != nums[i+1]:
+                nums[position] = nums[i+1]
+                position += 1
+            
+        return position
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 sol1 = Solution()
 nums = [0,0,1,1,1,2,2,3,3,4]
