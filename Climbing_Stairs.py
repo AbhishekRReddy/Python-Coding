15c7ab2c255eefe8fc6d00fa512e9df7675971f7
AbhishekRReddy <arreddy1997@gmail.com>
2023-04-09 12:55:13 +0530

    Climibing stairs solved using the python

---
+++ Climbing_Stairs.py
@@ -0,0 +1,21 @@
+'''
+https://leetcode.com/problems/climbing-stairs/description/
+Memoization 
+'''
+
+
+def stairs(n,dp):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    if n in dp:
+        return dp[n] 
+    left = stairs(n-1,dp)
+    right = stairs(n-2,dp)
+    dp[n] = left + right
+    return dp[n]
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = {}
+        return stairs(n,dp)
