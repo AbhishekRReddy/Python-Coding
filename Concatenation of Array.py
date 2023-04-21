f6eb7464a61dfd35df681efbb8c54e99362f09c7
AbhishekRReddy <arreddy1997@gmail.com>
2023-04-21 22:44:31 +0530

    Concatenation

--- Concatenation of Array.py
+++ Concatenation of Array.py
@@ -3,7 +3,7 @@ https://leetcode.com/problems/concatenation-of-array/description/
 '''
 
 class Solution:
-    def getConcatenation(self, nums: List[int]) -> List[int]:
+    def getConcatenation(self, nums: list[int]) -> list[int]:
         l = len(nums)
         ans = [0] * (2 *l)
         for i,n in enumerate(nums):
