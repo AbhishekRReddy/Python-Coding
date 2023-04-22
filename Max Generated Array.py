bcb4c85b5967b8d89cce9e1c4d6ed0f52f296165
AbhishekRReddy <arreddy1997@gmail.com>
2023-04-22 18:17:24 +0530

    Tabulation approach

--- Max Generated Array.py
+++ Max Generated Array.py
@@ -24,3 +24,20 @@ class Solution:
         for i in range(n,-1,-1):
             f(i,nums)
         return max(nums)
+    
+'''
+Tabulation
+'''
+class Solution:
+    def getMaximumGenerated(self, n: int) -> int:
+        nums = [-1] * (n+1)
+        nums[0] = 0
+        if n == 0:
+            return nums[0]
+        nums[1] = 1
+        for i in range(2,n+1):
+            if i % 2 == 0:
+                nums[i] = nums[i//2]
+            else:
+                nums[i] = nums[i//2] + nums[(i//2)+1]
+        return max(nums)
