72f958ff1facaae8c4f1a2e5a98b51160e50ed2b
AbhishekRReddy <arreddy1997@gmail.com>
2023-04-27 22:32:58 +0530

    Set_Matrix_Zeroes with O(n^2) solution

--- Set_Matrix_Zeroes.py
+++ Set_Matrix_Zeroes.py
@@ -2,8 +2,6 @@
 Set the row and column of a element equal to zero, if the element is equal to zero.
 Brute Force Approach
 '''
-
-
 def setZeros(matrix: list[list[int]]) -> None:
 	# Write your code here.
     row = len(matrix)
@@ -18,4 +16,29 @@ def setZeros(matrix: list[list[int]]) -> None:
             matrix[ind[0]][c] = 0
         for r in range(row):
             matrix[r][ind[1]] = 0
+    return matrix
+
+'''
+Optimized
+'''
+def setZeros(matrix: list[list[int]]) -> None:
+	# Write your code here.
+    row = len(matrix)
+    col = len(matrix[0])
+    r = [0] * col
+    c = [0] * row
+    for i in range(row):
+        for j in range(col):
+            if matrix[i][j] == 0:
+                r[j] = 1
+                c[i] = 1
+    for i in range(row):
+        for j in range(col):
+            if matrix[i][j] != 0:
+                if r[j] == 1:
+                    matrix[i][j] = 0
+                    continue
+                if c[i] == 1:
+                    matrix[i][j] = 0
+                    continue
     return matrix
