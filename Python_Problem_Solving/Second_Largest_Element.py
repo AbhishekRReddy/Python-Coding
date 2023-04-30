81a0847df9ce1e75bfa00350137d1c2c6792a552
AbhishekRReddy <arreddy1997@gmail.com>
2023-04-30 20:58:36 +0530

    Second_Largest_Element is set as -1 in the beginning

--- Python_Problem_Solving/Second_Largest_Element.py
+++ Python_Problem_Solving/Second_Largest_Element.py
@@ -2,16 +2,12 @@ class Solution:
 
     def print2largest(self,arr:list, n):
     # code here
-        if n == 1:
-            return -1
         first_max = arr[0]
-        second_max = float('-inf')
+        second_max = -1
         for i in range(1,len(arr)):
             if arr[i] > first_max:
                 second_max = first_max
                 first_max = arr[i]
             elif arr[i] > second_max and arr[i] < first_max:
                 second_max = arr[i]
-        if second_max == float('-inf'):
-            return -1
         return second_max  
