6a1c4759646b5a7b6fcaa163662df04e619e474c
AbhishekRReddy <arreddy1997@gmail.com>
2023-04-30 22:26:21 +0530

    Without using the inbuilt methods

--- Python_Problem_Solving/Rotate_Array.py
+++ Python_Problem_Solving/Rotate_Array.py
@@ -9,3 +9,21 @@ class Solution:
             temp = nums.pop()
             nums.insert(0,temp)
         return nums
+
+'''
+Without using the inbuilt methods
+'''
+class Solution:
+    def rotate(self, nums: list[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        temp = [nums[i] for i in range(len(nums)-k,len(nums))]
+        print(nums)
+        for i in range(len(nums)-k-1,-1,-1):
+            nums[i+k] = nums[i]
+        print(nums)
+        for i in range(0,k):
+            nums[i] = temp[i]
+        
