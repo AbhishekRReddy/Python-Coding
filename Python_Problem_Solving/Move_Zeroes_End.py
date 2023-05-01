9fcd14a1e7785e16f9318a44db30b38b0096c829
AbhishekRReddy <arreddy1997@gmail.com>
2023-05-01 11:39:59 +0530

    Better with Swapping

--- Python_Problem_Solving/Move_Zeroes_End.py
+++ Python_Problem_Solving/Move_Zeroes_End.py
@@ -17,3 +17,24 @@ class Solution:
             no_z += 1
         for i in range(len(nums)-no_z,len(nums)):
             nums[i] = 0
+
+'''
+Better with Swapping
+'''
+class Solution:
+    def moveZeroes(self, nums: list[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        pos = -1
+        for i,num in enumerate(nums):
+            if num == 0:
+                pos = i
+                break
+        if pos == -1:
+            return
+        for j in range(pos+1,len(nums)):
+            if nums[j] != 0:
+                nums[pos],nums[j] = nums[j],nums[pos]
+                pos += 1
+                continue
